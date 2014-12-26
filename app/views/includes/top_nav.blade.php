14b4b2c5bef5c5ebab28d15e7221291d7e928fa8
phpdeveloper02 <imran.intuch@gmail.com>
2014-12-26 14:20:36 -0800

    new front templates

--- app/views/includes/top_nav.blade.php
+++ app/views/includes/top_nav.blade.php
@@ -16,11 +16,11 @@
                                 <li>
                                 	<a href="#">ABOUT US</a>
                                 	<ul>                                  
-                                        <li><a href="corporate_profile.html">Corporate Profile</a></li>
-                                        <li><a href="vision_mission.html">Vision &amp; Mission</a></li>
-                                        <li><a href="board_of_directors.html">Board of Directors</a></li>
-                                        <li><a href="corporate_structure.html">Corporate Structure</a></li>
-                                        <li><a href="milestone.html">Milestone</a></li>
+                                        <li><a href="{{ URL::to('/pages/corporate_profile')}}">Corporate Profile</a></li>
+                                        <li><a href="{{ URL::to('/pages/vision_mission')}}">Vision &amp; Mission</a></li>
+                                        <li><a href="{{ URL::to('/pages/board_of_directors')}}">Board of Directors</a></li>
+                                        <li><a href="{{ URL::to('/pages/corporate_structure')}}">Corporate Structure</a></li>
+                                        <li><a href="{{ URL::to('/pages/milestone')}}">Milestone</a></li>
                                         <li><a href="{{ URL::to('/pages/achievements')}}">Achievements</a></li>
                                         <li><a href="{{ URL::to('/pages/board_charter')}}">Board Charter</a></li>
                                         <li><a href="{{ URL::to('/pages/news_events') }}">News &amp; Events</a></li>
@@ -34,46 +34,46 @@
                                         
                                     </ul>
                                 </li>
-                                <li><a href="our_clients.html">CLIENTS</a></li>
+                                <li><a href="{{ URL::to('/pages/our_clients')}}">CLIENTS</a></li>
                                  <li>
-                                    <a href="investor_relations_home.html">INVESTOR RELATIONS</a>
+                                    <a href="{{ URL::to('/pages/investor_relations_home')}}">INVESTOR RELATIONS</a>
                                     <ul>                                  
                                         <li><a href="#">Corporate Information</a>
                                         	<ul>
-                                            	<li><a href="corporate_info_general.html">General</a></li>
-                                                <li><a href="corporate_info_directors_profile.html">Director's Profile</a></li>
-                                                <li><a href="corporate_info_key_mgmt_team.html">Key Management Team</a></li>
-                                                <li><a href="corporate_info_our_properties.html">Our Properties</a></li>
-                                                <li><a href="corporate_info_our_subsidiaries.html">Our Subsidiaries</a></li>
+                                            	<li><a href="{{ URL::to('/pages/corporate_info_general')}}">General</a></li>
+                                                <li><a href="{{ URL::to('/pages/corporate_info_directors_profile')}}">Director's Profile</a></li>
+                                                <li><a href="{{ URL::to('/pages/corporate_info_key_mgmt_team')}}">Key Management Team</a></li>
+                                                <li><a href="{{ URL::to('/pages/corporate_info_our_properties')}}">Our Properties</a></li>
+                                                <li><a href="{{ URL::to('/pages/corporate_info_our_subsidiaries')}}">Our Subsidiaries</a></li>
                                             </ul>
                                         </li>
-                                        <li><a href="corporate_governance.html">Corporate Governance</a></li>
+                                        <li><a href="{{ URL::to('/pages/corporate_governance')}}">Corporate Governance</a></li>
                                         <li><a href="#">IPO Centre</a>
                                         	<ul>
-                                            	<li><a href="ipo_centre_ipo_statistics.html">IPO Statistics</a></li>
-                                                <li><a href="ipo_centre_competitive_advantages.html">Competitive Advantages</a></li>
-                                                <li><a href="ipo_centre_risk_factors.html">Risk Factors</a></li>
-                                                <li><a href="ipo_centre_utilisation_proceeds.html">Utilisation of Proceeds</a></li>
-                                                <li><a href="ipo_centre_industry_overview.html">Industry Overview</a></li>
+                                            	<li><a href="{{ URL::to('/pages/ipo_centre_ipo_statistics')}}">IPO Statistics</a></li>
+                                                <li><a href="{{ URL::to('/pages/ipo_centre_competitive_advantages')}}">Competitive Advantages</a></li>
+                                                <li><a href="{{ URL::to('/pages/ipo_centre_risk_factors')}}">Risk Factors</a></li>
+                                                <li><a href="{{ URL::to('/pages/ipo_centre_utilisation_proceeds')}}">Utilisation of Proceeds</a></li>
+                                                <li><a href="{{ URL::to('/pages/ipo_centre_industry_overview')}}">Industry Overview</a></li>
                                             </ul>
                                         </li>
                                         <li><a href="#">Share Information</a>
                                         	<ul>
-                                            	<li><a href="share_information_price_ticker.html">Price Ticker</a></li>
-                                                <li><a href="share_information_stock_charts.html">Stock Charts</a></li>
-                                                <li><a href="share_information_price_volume.html">Price &amp; Volume</a></li>
-                                                <li><a href="share_information_analysis_shareholdings.html">Analysis of Shareholdings</a></li>
-                                                <li><a href="share_information_top_30_shareholders.html">Top 30 Shareholders</a></li>
+                                            	<li><a href="{{ URL::to('/pages/share_information_price_ticker')}}">Price Ticker</a></li>
+                                                <li><a href="{{ URL::to('/pages/share_information_stock_charts')}}">Stock Charts</a></li>
+                                                <li><a href="{{ URL::to('/pages/share_information_price_volume')}}">Price &amp; Volume</a></li>
+                                                <li><a href="{{ URL::to('/pages/share_information_analysis_shareholdings')}}">Analysis of Shareholdings</a></li>
+                                                <li><a href="{{ URL::to('/pages/share_information_top_30_shareholders')}}">Top 30 Shareholders</a></li>
                                             </ul>
                                         </li>
-                                        <li><a href="entitlements.html">Entitlements</a></li>
+                                        <li><a href="{{ URL::to('/pages/entitlements')}}">Entitlements</a></li>
                                         <li><a href="#">Financial Information</a>
                                         	<ul>
-                                            	<li><a href="financial_information_financial_highlights.html">Financial Highlights</a></li>
-                                                <li><a href="financial_information_comprehensive_income.html">Comprehensive Income</a></li>
-                                                <li><a href="financial_information_financial_position.html">Financial Position</a></li>
-                                                <li><a href="financial_information_cash_flow_statement.html">Cash Flow Statement</a></li>
-                                                <li><a href="financial_information_changes_in_equity.html">Statement of Changes In Equity</a></li>
+                                            	<li><a href="{{ URL::to('/pages/financial_information_financial_highlights')}}">Financial Highlights</a></li>
+                                                <li><a href="{{ URL::to('/pages/financial_information_comprehensive_income')}}">Comprehensive Income</a></li>
+                                                <li><a href="{{ URL::to('/pages/financial_information_financial_position')}}">Financial Position</a></li>
+                                                <li><a href="{{ URL::to('/pages/financial_information_cash_flow_statement')}}">Cash Flow Statement</a></li>
+                                                <li><a href="{{ URL::to('/pages/financial_information_changes_in_equity')}}">Statement of Changes In Equity</a></li>
                                                 <li><a href="financial_information_latest_quarterly_report.html">Latest Quarterly Report</a></li>
                                                 <li><a href="financial_information_segmental_analysis.html">Segmental Analysis</a></li>
                                                 <li><a href="financial_information_ratio_analysis.html">Ratio Analysis</a></li>
